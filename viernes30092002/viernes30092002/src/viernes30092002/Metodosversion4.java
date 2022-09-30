d7f77e5d963e75da685184ee617dfb8a2c938127
keneth2002 <111157800+keneth2002@users.noreply.github.com>
2022-09-30 17:45:00 -0600

    metodos

---
+++ viernes30092002/viernes30092002/src/viernes30092002/Metodosversion4.java
@@ -0,0 +1,49 @@
+package viernes30092002;
+
+import java.util.Scanner;
+
+public class Metodosversion4 {
+
+	public static void main(String[] args) {
+		
+		
+		new Metodosversion4().calcularRaicesCuadraticas();
+		new Metodosversion4().calcularRaicesCuadraticas();
+		
+     
+	}//cierre del main
+	
+	//modificador de accesos + 
+	//tipo de datos que va a devolver el el metodo + 
+	//nombre del metodo + los parentesis() +
+	//y/o lista de parametros de entrada
+	
+	public static  void calcularRaicesCuadraticas() {
+		 Scanner lector = new Scanner(System.in);
+		//cuerpo del metodo
+		   System.out.println("a = ");
+	       double a = lector.nextDouble();
+	       System.out.println("b = ");
+	       double b = lector.nextDouble();
+	       System.out.println("c = ");
+	       double c = lector.nextDouble();
+	       
+	       double discriminante = Math.pow(b, 2) - 4 * a * c;
+	       
+	       double x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+	       double x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+	       
+	       System.out.println("x1 = " + x1);
+	       System.out.println("x2 = " + x2);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}//cierre de la clase
---
+++ viernes30092002/viernes30092002/src/viernes30092002/PruebasdeMetodos.java
@@ -0,0 +1,10 @@
+package viernes30092002;
+
+public class PruebasdeMetodos {
+
+	public static void main(String[] args) {
+		new Metodosversion3().calcularRaicesCuadraticas();
+		Metodosversion3.calcularRaicesCuadraticas();
+	}
+
+}
